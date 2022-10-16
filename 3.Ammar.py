cdcabadba56076a20856067af1c49308bebf0923
BigAmmar <113592017+BigAmmar@users.noreply.github.com>
2022-10-16 18:34:27 +0200

    16.10.2022
    Upeava 3.priklad a aritmetický průměr čísel v libovolném seznamu

--- 3.Ammar.py
+++ 3.Ammar.py
@@ -1,9 +1,11 @@
 x = int(input('zadejte liche cislo:'))
 
-
+liche_cisla=[]
 liche_cislo = 1
+
 for i in range(7):
     liche_cislo = liche_cislo +2
-    print(liche_cislo)
-#nemam dokoncene
-#uplne by stacilo ten print vyhodit z cyklu  
+    liche_cisla.append(liche_cislo)
+
+scitani = sum(liche_cisla)+x
+print(sum(liche_cisla),'+',x,'=',scitani)
---
+++ APCAmmar.py
@@ -0,0 +1,2 @@
+sez = [8,9,2,5,7,6,14]
+print('peumer cisla v seznamu je:',sum(sez)/len(sez))
